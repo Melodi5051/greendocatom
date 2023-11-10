20125f59d592bc1891c92e2d1e4d7e55b1f27afc
gramizor <granin17@yandex.ru>
2023-11-10 20:34:27 +0300

    updated header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import './Header.css'
 import '../../index.css'
 
 const Header = () => {
-  const [isLogged, setIsLogged] = useState(false);
+  const [isLogged, setIsLogged] = useState(true);
   const [isMenuOpen, setMenuOpen] = useState(false);
 
   const toggleMenu = () => {
@@ -13,11 +13,12 @@ const Header = () => {
 
   return (
     <div className="navbar">
-      <img src="/img/ГРИНАТОМ_Лого2021 2.png" alt="logo2" className="logotype" />
-      <div className="links">
-        <NavLink className='nav-link' to={"/documents"}>Документы</NavLink>
-        <NavLink className='nav-link' to={"/categories"}>Категории</NavLink>
-        <NavLink className='nav-link' to={"/userlist"}>Список пользователей</NavLink>
+      <div className="logo">
+        <img src="/img/ГРИНАТОМ_Лого2021 1.png" alt="logo" className="logotype" />
+        <div className="text">
+          <h1>гринатом</h1>
+          <h2>Документы</h2>
+        </div>
       </div>
       <div className="authorization">
         {isLogged ? (
