fc1f8252f321ec3e7ec31a832ea42543d5df1cd4
Anavuajna <anavuajna@yandex.ru>
2023-11-10 22:46:29 +0300

    Footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import '../../index.css'
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <p id='copiright'>©️ команда 1. 2023 год</p>
+            <a id='devs' href='#dev'>список разработчиков</a>
+        </div>
+    )
+}
+
+export default Footer;
