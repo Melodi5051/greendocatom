343e67e01d0a9113636b52c5169cf4393b01e295
gramizor <granin17@yandex.ru>
2023-11-10 19:43:37 +0300

    created header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import React, { useEffect } from 'react';
 import { observer } from 'mobx-react-lite';
 import { appStore } from './store/store';
-import {getAllFiles} from './API/axios.api'
+import { getAllFiles } from './API/axios.api'
+import Header from './components/Header/Header';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 const App: React.FC = () => {
   useEffect(() => {
     getAllFiles();
-  }, []); 
+  }, []);
   return (
     <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          {/* <Route path="/documents" element={<Documents/>} /> */}
+          {/* <Route path="/categories" element={<Categories/>} /> */}
+          {/* <Route path="/userlist" element={<UserList/>} /> */}
+        </Routes>
+      </BrowserRouter>
       <h1>Files from Yandex.Disk</h1>
       <ul>
         {appStore.arrayItems.map((file, index) => (
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,40 @@
-import React from "react"
+import React, { useState } from "react"
+import { NavLink } from "react-router-dom"
+import './Header.css'
+import '../../index.css'
 
 const Header = () => {
-  return <div></div>
+  const [isLogged, setIsLogged] = useState(false);
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <div className="navbar">
+      <img src="/img/ГРИНАТОМ_Лого2021 2.png" alt="logo2" className="logotype" />
+      <div className="links">
+        <NavLink className='nav-link' to={"/documents"}>Документы</NavLink>
+        <NavLink className='nav-link' to={"/categories"}>Категории</NavLink>
+        <NavLink className='nav-link' to={"/userlist"}>Список пользователей</NavLink>
+      </div>
+      <div className="authorization">
+        {isLogged ? (
+          <div className="loggedIn">
+            <img src="img/Customer.png" alt="userIcon" className="customer" />
+            <div className="user-name">
+              {"Имя"} {"Фамилия"}
+            </div>
+          </div>
+        ) : (
+          <div className="unloggined">
+            <button className="login">Войти</button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Header
