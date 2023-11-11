c77862f9c3659e8f072ea081f5cd7a233eeea821
MblLLI16 <mbllli16@mail.ru>
2023-11-11 12:30:19 +0800

    added create folder, delete folder request

--- src/API/axios.api.ts
+++ src/API/axios.api.ts
@@ -1,7 +1,8 @@
-import axios from "axios"
+import axios, { isAxiosError } from "axios"
 import { appStore } from "../store/store"
 import { extractFolderName, formatData } from "../helper/formatDate"
 import { IYandexDiskFile } from "../types/Files"
+import { error } from "console"
 export const getAllFiles = async () => {
   try {
     const response = await axios.get("https://cloud-api.yandex.net/v1/disk/resources/files", {
@@ -25,13 +26,52 @@ export const getAllFiles = async () => {
   }
 }
 
-export const createFolders = () => {
-  return ""
+export const createFolders = async (folderName: string) => {
+
+  try {
+    const response = await axios.put("https://cloud-api.yandex.net/v1/disk/resources", {
+      headers: {
+        Authorization: process.env.REACT_APP_API_KEY,
+      },
+      params: {
+        path: `CaseLabDocuments/${folderName}`
+      }
+    }
+    );
+    console.log('создали успешно');
+  } catch (error) {
+    if (isAxiosError(error) && error.response && error.response.status === 409) {
+      console.log(`Директория ${folderName} уже существует`);
+    } else {
+      console.error("Ошибка АПИ запроса", error);
+    }
+  }
+};
+
+// delete применяется и к папкам, и к файлам
+export const deleteResources = async (resourceName: string) => {
+
+  try {
+    const response = await axios.delete("https://cloud-api.yandex.net/v1/disk/resources", {
+      headers: {
+        Authorization: process.env.REACT_APP_API_KEY,
+      },
+      params: {
+        path: `CaseLabDocuments/${resourceName}`,
+        force_async: true
+      },
+    });
+    console.log('удалили успешно');
+  } catch (error) {
+    console.error("Ошибка АПИ запроса", error)
+  }
 }
+
+
 
 export const createFiles = () => {
   return ""
 }
 
-export const getFolder = async (currentPath: string) => {}
-export const getFolderContents = async (folderPath: string) => {}
+export const getFolder = async (currentPath: string) => { }
+export const getFolderContents = async (folderPath: string) => { }
--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,58 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import { observer } from 'mobx-react-lite';
 import { appStore } from './store/store';
-import {getAllFiles} from './API/axios.api'
+import { getAllFiles, createFolders, deleteResources } from './API/axios.api';
 const App: React.FC = () => {
-  useEffect(() => {
+  const [folderName, setFolderName] = useState('');
+
+  // Создать папку
+  const handleCreateFolder = () => {
+    createFolders(folderName);
+  };
+
+  // Удалить папку или файл
+  const handleDeleteResource = () => {
+    deleteResources(folderName);
+  };
+
+  // Получить список файлов
+  const handleGetAllFiles = () => {
     getAllFiles();
-  }, []); 
+  };
+
   return (
     <div>
       <h1>Files from Yandex.Disk</h1>
+
+      <div>
+        <label>
+          Folder Name:
+          <input type="text" value={folderName} onChange={(e) => setFolderName(e.target.value)} />
+        </label>
+        <button onClick={handleCreateFolder}>Create Folder</button>
+        <button onClick={handleDeleteResource}>Delete Folder/File</button>
+        <button onClick={handleGetAllFiles}>Get All Files</button>
+      </div>
+
       <ul>
         {appStore.arrayItems.map((file, index) => (
           <li key={index}>
-            <strong>Название:</strong> {file.name}, <strong>Категория:</strong> {file.path}, <strong>Дата обновления:</strong> {file.modified}
+            <strong>Название:</strong> {file.name}, <strong>Категория:</strong> {file.path},{' '}
+            <strong>Дата обновления:</strong> {file.modified}
           </li>
         ))}
       </ul>
     </div>
+    // <div>
+    //   <h1>Files from Yandex.Disk</h1>
+    //   <ul>
+    //     {appStore.arrayItems.map((file, index) => (
+    //       <li key={index}>
+    //         <strong>Название:</strong> {file.name}, <strong>Категория:</strong> {file.path}, <strong>Дата обновления:</strong> {file.modified}
+    //       </li>
+    //     ))}
+    //   </ul>
+    // </div>
   );
 };
 
