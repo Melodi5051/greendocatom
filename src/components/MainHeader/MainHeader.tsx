ab80a9eac749ffd9d4b1248b9c95b3829a26016b
einz <iljapopov01@mail.ru>
2023-11-19 14:59:19 +0300

    Fix refresh icon logic, add animation. Edit style aside button (click area)

--- src/components/MainHeader/MainHeader.tsx
+++ src/components/MainHeader/MainHeader.tsx
@@ -1,21 +1,28 @@
-import React from "react"
+import React, {useState} from "react"
 import "./MainHeader.css"
-import { observer } from "mobx-react-lite"
-import { appStore } from "../../store/store"
+import {observer} from "mobx-react-lite"
+import {appStore} from "../../store/store"
 import refresh_icon from "./../../assets/icons/icon-refreshpage.svg"
 import CustomInput from "../CustomInput/CustomInput"
 import {} from "../../API/axios.api"
 import arrayBack from "./../../assets/icons/icon-back.svg"
-import { Link, useLocation } from "react-router-dom"
-import { getAllFiles, getAllFolders } from "../../API/apiGetAll"
+import {Link, useLocation} from "react-router-dom"
+import {getAllFiles, getAllFolders, getAllTrash} from "../../API/apiGetAll"
 
 const MainHeader = () => {
   const location = useLocation()
-  const handleRefreshTable = () => {
-    getAllFiles()
+  const [loading, setLoading] = useState(false)
+  const handleRefreshTable = async () => {
+    setLoading(true)
     getAllFolders()
-    appStore.setCategoryFilter("")
+    if (location.pathname === "/basket") {
+      await getAllTrash()
+    } else (
+      await getAllFiles()
+    )
+
     appStore.setSubstring("")
+    setLoading(false)
   }
   return (
     <div className="main-header">
@@ -29,24 +36,24 @@ const MainHeader = () => {
             />
           </Link>
         ) : null}
-        <Link to={"/"}>
-          <img
-            className="refresh_icon"
-            src={refresh_icon}
-            alt=""
-            onClick={(event) => handleRefreshTable()}
-          />
-        </Link>
+
+        <img
+          className={'refresh_icon' + (loading ? ' refresh_icon_active' : '')}
+          src={refresh_icon}
+          alt=""
+          onClick={(event) => handleRefreshTable()}
+        />
+
         <h1>
           {appStore.categoryFilter.length > 0
             ? appStore.categoryFilter
             : location.pathname === "/basket"
-            ? "Удаленные файлы"
-            : "Все файлы"}
+              ? "Удаленные файлы"
+              : "Все файлы"}
         </h1>
       </div>
       <div className="main-input">
-        <CustomInput placeholder="Поиск документа" showButton={true} />
+        <CustomInput placeholder="Поиск документа" showButton={true}/>
       </div>
     </div>
   )
